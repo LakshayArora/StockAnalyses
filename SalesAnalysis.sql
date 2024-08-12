13d960724bc5e19242d47fc286e80d8e3ae8cf52
Lakshay <lakshayarora10@gmail.com>
2024-08-12 20:37:08 +1000

    git commit - m 'message'

---
+++ SalesAnalysis.sql
@@ -0,0 +1,12 @@
+/*
+. Amazon Sales Report
+Total Sales: What is the total sales amount for orders marked as "Shipped"?
+Top Selling Products: Which products (SKU) generated the most revenue?
+Category Performance: How does the revenue split across different product categories?
+Sales by Fulfillment Channel: What is the revenue distribution between different fulfillment channels (e.g., Amazon, Merchant)?
+Return Rate: How many orders were canceled, and what percentage does this represent of total orders?
+*/
+
+Select *
+From amazon_sales
+Limit 5
